03c4b88309c0c89a8627727b4b97254cfc43c648
ashutosh <ashutosh123ojha@gmail.com>
2017-11-29 02:38:10 +0530

    cherry picked

--- app/src/main/java/com/example/admin/ashutosh/SubActivity.java
+++ app/src/main/java/com/example/admin/ashutosh/SubActivity.java
@@ -1,5 +1,7 @@
 package com.example.admin.ashutosh;
 
+import android.util.Log;
+
 /**
  * Created by Admin on 11/1/2017.
  */
@@ -10,5 +12,10 @@ public class SubActivity {
     void methodDivide(){
         int x=8,y=8;
         int z=x/y;
+        
+
+        Log.e("Tag",z+"0");
+
+
     }
 }
